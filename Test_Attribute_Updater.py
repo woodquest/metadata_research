91bff8946dd6202c74073a1fda4206cb7c572946
woodquest <victoradejola@gmail.com>
2019-03-19 22:16:54 +0000

    Add files via upload

---
+++ Test_Attribute_Updater.py
@@ -0,0 +1,9 @@
+from Attribute_Updater.Header import Header
+from Attribute_Updater.Dataset import Dataset
+
+newHeader = Header()
+newHeader.printheaders()
+
+newDataset = Dataset()
+newDataset.printdatasets()
+
